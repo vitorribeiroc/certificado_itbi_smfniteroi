6d48214d364467dde61e1bce16c5600d16764fcd
vitorribeiroc <97795826+vitorribeiroc@users.noreply.github.com>
2025-07-16 17:11:48 -0300

    Update run app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def gerar_documento():
     return send_file(caminho_saida, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
